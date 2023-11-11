c54c98e9c829705b0b5ac45d8dbd04541bb0ebc0
mswtUIOWA <123667765+mswtUIOWA@users.noreply.github.com>
2023-11-10 20:48:57 -0600

    SQL and php code that added new entry

--- employee.php
+++ employee.php
@@ -10,69 +10,43 @@
         Name: <input type="text" name="name" required><br>
         Date Hired: <input type="date" name="dateHired" required><br>
         Date of Birth: <input type="date" name="dob" required><br>
-        SSN: <input type="text" name="snn" required><br>
+        SSN: <input type="text" name="ssn" required><br>
         Gender: <input type="text" name="gender" required><br>
         Job: <input type="text" name="job" required><br>
         <input type="submit" value="Submit">
     </form>
-    <br>
-    <br>
-    <h1>Enter Employee Contact Info</h1>
-    <form method="post">
-        Address: <input type="text" name="address" required><br>
-        Phone Number: <input type="text" name="phone" required><br>
-        Email: <input type="text" name="email" required><br>
-        Emergency Contact Number: <input type="text" name="emergencyContact" required><br>
-        <input type="submit" value="Submit">
-    </form>
-    <br>
-    <br>
-    <h1>Enter Employee Salary Info</h1>
-    <form method="post">
-        Salary Amount: <input type="text" name="salary" required><br>
-        Pay Period: <input type="text" name="payPeriod" required><br>
-        Start Date: <input type="date" name="startDate" required><br>
-        End Date: <input type="text" name="endDate" required><br>
-        Bonus/Deduction: <input type="text" name="varPay" required><br>
-        <input type="submit" value="Submit">
-    </form>
-    <br>
-    <br>
-    <!--need to add post employment, schedule, performance, and experience entries-->
 </body>
 </html>
 
 
-<?
-// from previous assignment
-/*
+<?php
 $servername = "localhost";
-$username = "pi";
-$password = "raspberry";
-$dbname = "DVM";
+$username = "root";
+$password = "";
+$dbname = "phpmyadmin";
 
 # Connect to db
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if (!$conn){
+    echo 'Not Connected';
+}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Retrieve data from the HTML form
-    $license = $_POST['license'];
-    $state = $_POST['state'];
-    $last = $_POST['last'];
-    $first = $_POST['first'];
-    $phone = $_POST['phone'];
-    $email = $_POST['email'];
+    $name = $_POST['name'];
+    $dateHired = $_POST['dateHired'];
+    $dob = $_POST['dob'];
+    $ssn = $_POST['ssn'];
+    $gender = $_POST['gender'];
+    $job = $_POST['job'];
 
     // Prepare and execute the SQL statement
-    $stmt = $conn->prepare("INSERT INTO Persons (License, State, Last, First, Phone, Email) VALUES ('$license', '$state', '$last', '$first', '$phone', '$email')");
-    if ($stmt->execute()) {
-        // Redirect to the second PHP file (car.php)
-        header("Location: car.php");
-        exit;
+    //fix snn
+    $sql = "INSERT INTO employee (Name, DateHired, DOB, SNN, Gender, Job) VALUES ('$name', '$dateHired', '$dob', '$ssn', '$gender', '$job')";
+    if ($conn->query($sql) === TRUE) {
+        echo "Record inserted successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
     }
-    // Close the statement and database connection
-    $stmt->close();
-    $conn->close();
 }
-*/
 ?>
